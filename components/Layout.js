33bd17dbb4bf4af5fb116829322a9d17ca8473b8
Michael Rispoli <mrispoli24@gmail.com>
2018-11-15 17:34:12 -0500

    learning the hard way not to deal with css or sass with this stack

--- components/Layout.js
+++ components/Layout.js
@@ -1,19 +1,14 @@
 import Head from 'next/head'
 import Header from './Header'
 
-const layoutStyle = {
-  margin: 20,
-  padding: 20,
-  border: '1px solid #DDD'
-}
-
 const Layout = (props) => (
-  <div style={layoutStyle}>
+  <div className="example">
     <Head>
         <title>{props.title}</title>
         <meta charSet='utf-8' />
         <meta name='viewport' content='initial-scale=1.0, width=device-width' />
         <meta name='description' content={props.description} />
+        <link href="/static/styles/global.css" rel="stylesheet" />
     </Head>
     <Header />
     {props.children}
